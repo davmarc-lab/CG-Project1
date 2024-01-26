0b26eaf4bef9f3ddef7e30cb225e4d18aac2838d
Davide Marchetti <davide.marchetti6@studio.unibo.it>
2024-01-26 15:05:07 +0100

    little modification to the lib.hpp file

--- CG-Project1/Lib.hpp
+++ CG-Project1/Lib.hpp
@@ -1,6 +1,6 @@
 #define GLM_ENABLE_EXPERIMENTAL         // due to glm recent updates sometimes is needed this line
 #include "include/glad/glad.h"
-#include <GLFW/glfw3.h>
+#include "include/GLFW/glfw3.h"
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/matrix_access.hpp>
